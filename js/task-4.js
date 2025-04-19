46565438ea089fbb059a1c943b579c42efb63dd3
Andriy Goloviy <kykyagoloviy@gmail.com>
2025-04-19 19:53:11 +0300

    Add files via upload

--- js/task-4.js
+++ js/task-4.js
@@ -1,17 +1,27 @@
-function checkForSpam(message) {
-  const normalizedMessage = message.toLowerCase();
-  if (normalizedMessage.includes("spam") || normalizedMessage.includes("sale")) {
-    return true; 
-  } else {
-    return false; 
+function getShippingCost(country) {
+  let price;
+  switch (country) {
+    case "China":
+      price = 100;
+      return `Shipping to ${country} will cost ${price} credits`;
+    case "Chile":
+      price = 250;
+      return `Shipping to ${country} will cost ${price} credits`;
+    case "Australia":
+      price = 170;
+      return `Shipping to ${country} will cost ${price} credits`;
+    case "Jamaica":
+      price = 120;
+      return `Shipping to ${country} will cost ${price} credits`;
+    default:
+      return "Sorry, there is no delivery to your country";
   }
 }
 
 // Перевірка функції
-console.log(checkForSpam("Останні новини технологій")); // false
-console.log(checkForSpam("щотижневий бюлетень JavaScript")); // false
-console.log(checkForSpam("Отримайте найкращі пропозиції продажу зараз!")); // true
-console.log(checkForSpam("Чудовий SalE, тільки сьогодні ввечері!")); // true
-console.log(checkForSpam("Повірте мені, це не спам")); // true
-console.log(checkForSpam("Позбудьтеся спам-листів. Наша книга в продажу!")); // true
-console.log(checkForSpam("[СПАМ] Як швидко заробити?")); // true
+console.log(getShippingCost("Австралія")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("Німеччина")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Швеція")); // "Sorry, there is no delivery to your country"
