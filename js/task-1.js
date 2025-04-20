aedc7bcdce7aba1f61e1cde5f252de5f5548dac3
Andriy Goloviy <kykyagoloviy@gmail.com>
2025-04-20 08:58:29 +0300

    Add files via upload

--- js/task-1.js
+++ js/task-1.js
@@ -2,15 +2,15 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
   const totalPrice = quantity * pricePerDroid;
   if (totalPrice > customerCredits) {
-    return "Недостатньо коштів!";
+    return "Insufficient";
   } else {
-    return `Ви замовили ${quantity} дроїдів вартістю ${totalPrice} кредитів!`;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
   }
 }
 
-// Перевірка функції
-console.log(makeTransaction(5, 3000, 23000)); // "Ви замовили 5 дроїдів вартістю 15000 кредитів!"
-console.log(makeTransaction(3, 1000, 15000)); // "Ви замовили 3 дроїдів вартістю 3000 кредитів!"
-console.log(makeTransaction(10, 5000, 8000)); // "Недостатньо коштів!"
-console.log(makeTransaction(8, 2000, 10000)); // "Недостатньо коштів!"
-console.log(makeTransaction(10, 500, 5000));  // "Ви замовили 10 дроїдів вартістю 5000 кредитів!"
+
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
--- js/task-3.js
+++ js/task-3.js
@@ -8,11 +8,11 @@ function checkForSpam(message) {
     return false; 
   }
 }
-// Перевірка функції
-console.log(checkForSpam("Останні новини технологій")); // false
-console.log(checkForSpam("щотижневий бюлетень JavaScript")); // false
-console.log(checkForSpam("Отримайте найкращі пропозиції продажу зараз!")); // true
-console.log(checkForSpam("Чудовий SalE, тільки сьогодні ввечері!")); // true
-console.log(checkForSpam("Повірте мені, це не спам")); // true
-console.log(checkForSpam("Позбудьтеся спам-листів. Наша книга в продажу!")); // true
-console.log(checkForSpam("[СПАМ] Як швидко заробити?")); // true
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
